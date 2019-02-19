21031d5f245f04d29b06541e1e09f8029e976618
David Konsumer <konsumer@jetboystudio.com>
2019-02-19 11:32:40 -0800

    write files, not console.log

--- packages/cli/crudql.js
+++ packages/cli/crudql.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
+const fs = require('fs')
+const { promisify } = require('util')
+const path = require('path')
+
+const writeFile = promisify(fs.writeFile)
 
 const { getType } = require('./lib/utils')
 const schemaCommand = require('./lib/schema')
@@ -10,16 +15,44 @@ const argv = yargs
   .usage('Usage: $0 <command> [options]')
   .demandCommand(1)
 
-  .command('schema <file> <model>', 'Generate GraphQL schema for basic CRUD', yargs => {}, async argv => {
-    const { model, file } = argv
+  .command('schema <file> <model>', 'Generate GraphQL schema for basic CRUD', yargs => {
+    yargs.option('output', {
+      alias: 'o',
+      describe: 'The directory where this gets outputted',
+      default: 'schema'
+    })
+  }, async argv => {
+    const { model, file, output } = argv
     const type = await getType(file, model)
-    console.log(schemaCommand(type, file))
+    const filename = path.join(output, model.toLowerCase() + '_crud.graphql')
+    await writeFile(filename, schemaCommand(type, file))
+    console.log(`Wrote file: ${filename}`)
   })
 
-  .command('dynamo <file> <model>', 'Generate DynamoDB resolvers for basic CRUD schema', yargs => {}, async argv => {
-    const { model, file } = argv
+  .command('dynamo <file> <model>', 'Generate DynamoDB resolvers for basic CRUD schema', yargs => {
+    yargs.option('output', {
+      alias: 'o',
+      describe: 'The directory where this gets outputted',
+      default: 'resolvers'
+    })
+  }, async argv => {
+    const { model, file, output } = argv
     const type = await getType(file, model)
-    console.log(dynamoCommand(type, file))
+    const filename = path.join(output, model.toLowerCase() + '_crud.js')
+    await writeFile(filename, dynamoCommand(type, file))
+    console.log(`Wrote file: ${filename}`)
+  })
+
+  .command('reactstrap <file> <model>', 'Generate components in reactstrap for CRUD', yargs => {
+    yargs.option('output', {
+      alias: 'o',
+      describe: 'The directory where this gets outputted',
+      default: 'components'
+    })
+  }, async argv => {
+    const { model, file, output } = argv
+    const type = await getType(file, model)
+    // code goes here
   })
 
   .command('show <file> <model>', 'Just shows the model', yargs => {}, async argv => {
